781da229710a63aa207b5d7718f2d05f53d92e18
Giuseppe DiBella <gd46@njit.edu>
2015-05-30 15:19:08 -0400

    Fixed typo

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+angular.module('routerApp', [])
+
+	.controller('mainController', function(){
+		var vm = this;
+		vm.bigMessage = 'A smoothed sea never made a skilled sailor';
+	})
+
+	.controller('homeController', function(){
+		var vm = this;
+		vm.message = 'This is the home page.';
+	})
+
+	.controller('aboutController', function(){
+		var vm = this;
+		vm.message = 'This is on the about page';
+	})
+
+	.controller('contactController', function(){
+		var vm = this;
+		vm.message = 'This is on the contact page';
+	});
